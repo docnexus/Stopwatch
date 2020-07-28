8d49966e4ed6d81228e513218ebb2d80fdbbca6a
docnexus <drlamb1@gmail.com>
2020-07-28 11:55:11 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading;
+
+namespace Stopwatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stopwatch = new Stopwatch();
+            var cont = true;
+
+            while (cont == true)
+            {
+                Console.WriteLine("Welcome to Stopwatch! Press Start to Start, Press Stop to Stop, or Exit to exit");
+                var entry = Console.ReadLine();
+                switch (entry)
+                {
+                    case "Start":
+                        stopwatch.Start();
+                        break;
+                    case "Stop":
+                        stopwatch.Stop();
+                        Console.WriteLine(stopwatch.Report());
+                        break;
+                    case "Exit":
+                        cont = false;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid entry try again.");
+                        break;
+                }
+            }
+
+        }
+
+    }
+}
---
+++ Stopwatch.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Channels;
+
+namespace Stopwatch
+{
+    public class Stopwatch
+    {
+        private DateTime _startTime;
+        private DateTime _endTime;
+        private TimeSpan _duration;
+        private bool _hasStarted;
+        private bool _hasEnded;
+
+        public void Start()
+        {
+            if (_hasStarted == false)
+            {
+                _startTime = DateTime.Now;
+                _hasStarted = true;
+            }
+            else
+            {
+                throw new Exception("User has already started once, cannot start again");
+            }
+        }
+
+        public void Stop()
+        {
+            if (_hasStarted == true)
+            {
+                _endTime = DateTime.Now;
+                _hasEnded = true;
+            }
+            else
+            {
+                throw new Exception("User has pressed stop before a start event");
+            }
+            
+        }
+
+        public TimeSpan Report()
+        {
+            if (_hasEnded == true && _hasStarted == true)
+            {
+                _duration = _endTime - _startTime;
+                _hasStarted = false;
+                _hasEnded = false;
+                return _duration;
+            }
+            else
+            {
+                throw new Exception("Invalid State has been reached");
+            }
+
+
+        }
+
+    }
+
+}
